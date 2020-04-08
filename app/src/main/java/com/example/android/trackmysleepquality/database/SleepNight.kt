7aa8f683fca01982ceafcf8f53aec4d8b61dc900
Damian <damian.ogorek.1996@gmail.com>
2020-04-08 18:10:12 +0200

    Added Coroutines for Long-running Operations also added the click handlers on the buttons.

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -27,11 +27,11 @@ data class SleepNight(
         val nightId: Long = 0L,
 
         @ColumnInfo(name = "start_time_milli")
-        val startTimeMilli : Long = System.currentTimeMillis(),
+        val startTimeMilli: Long = System.currentTimeMillis(),
 
         @ColumnInfo(name = "end_time_milli")
-        val endTimeMilli: Long = startTimeMilli,
+        var endTimeMilli: Long = startTimeMilli,
 
         @ColumnInfo(name = "sleep_quality")
-        val sleepQuality: Int = -1
+        var sleepQuality: Int = -1
 )
